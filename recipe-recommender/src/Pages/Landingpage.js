ca617c6f2ef6b39997fe1ebe62e0cc7b98a3b1b4
Lehan Ouyang <lo739@nyu.edu>
2024-02-12 12:11:17 -0500

    add side navigation bar

---
+++ recipe-recommender/src/Pages/Landingpage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../Pages/Landingpage.css"
+import Left from '../Components/Left/Left'
+
+
+const Landingpage = ({}) => {
+  console.log("abc")
+  return (
+    <div className='interface'>
+    <div className="home">
+      <Left />
+      </div>
+      </div>
+  )
+}
+
+export default Landingpage
