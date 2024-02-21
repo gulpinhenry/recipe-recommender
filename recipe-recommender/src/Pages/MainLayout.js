fe2b07dc15eb2d2d26e44b9f394c5972da124c3b
Lehan Ouyang <lo739@nyu.edu>
2024-02-21 12:23:00 -0500

    complete Main layout wrapper

--- recipe-recommender/src/Pages/MainLayout.js
+++ recipe-recommender/src/Pages/MainLayout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "../Pages/MainLayout.css"
 import Left from '../Components/Left/Left'
+import Nav from '../Components/Navigation/Nav'
 
 
 const Landingpage = ({}) => {
   return (
     <div className='interface'>
+      <Nav />
     <div className="home">
       <Left />
       </div>
