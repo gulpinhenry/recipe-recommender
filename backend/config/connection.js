cd7404ca57265d52a12af898a242b8a1b3d71ba0
henry <henry.j.kam@gmail.com>
2024-03-06 22:09:42 -0500

    add link from FE to BE

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-
-console.log(process.env.MONGODB_URI)
 const n = mongoose.connect(process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 
-require('dotenv').config( { path: './.env' } )
-
+require('dotenv').config( { path: './backend/.env' } )
 
 const db = require('./config/connection');
 
@@ -13,19 +12,20 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../recipe-recommender/build')));
 }
 else {
-    app.use(cors());
+    // app.use(cors());
 }
 
-app.get('/message', (req, res) => {
-    res.json({ message: "Hello from server!" });
-});
-
+// app.get('/message', (req, res) => {
+//     res.json({ message: "Hello from server!" });
+// });
+  
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../recipe-recommender/build/index.html'));
+    res.sendFile(path.join(__dirname, '../recipe-recommender/build/index.html'));
 });
 
 
@@ -33,4 +33,8 @@ db.then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
     });
-  });
+});
+
+process.on("uncaughtException", function (err) {
+    console.log("Caught exception: " + err);
+});
