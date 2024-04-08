a5341d26a262fd2d6bf7023a91ad04b27bc6f40f
Henry Kam <henry.kam@genesys.com>
2024-04-08 13:44:59 -0400

    FE add catch all page

--- recipe-recommender/src/App.js
+++ recipe-recommender/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './PageLayout/MainLayout'
 import PostLayout from './PageLayout/PostLayout'
+import WrongPageLayout from './PageLayout/WrongPageLayout'
 
 function App() {
   return (
 <div>
   <div className='App'>
       <Routes>
-         <Route path='/' element={<MainLayout />} />
-        {/* <Route path='/create' element={<PostLayout />} /> */} 
+        <Route path='/' element={<MainLayout />} />
+        <Route path='/create' element={<PostLayout />} /> 
         {/* <Route path='/' element={<PostLayout />} /> */}
+        <Route path='*' element={<WrongPageLayout />} />
       </Routes>
-    </div>
+  </div>
 </div>
   );
 }
---
+++ recipe-recommender/src/PageLayout/WrongPageLayout.js
@@ -0,0 +1,14 @@
+// WrongPageLayout.js
+
+import React from 'react';
+
+const WrongPageLayout = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1 className="display-2">404 Page Not Found</h1>
+      <p>The page you are looking for does not exist or has been moved.</p>
+    </div>
+  );
+}
+
+export default WrongPageLayout;
