7e99a92c520693c0ca3bfb67ab4458a760943d7d
Lehan Ouyang <lo739@nyu.edu>
2024-04-28 23:58:02 -0400

    fix comment name issue

--- recipe-recommender/src/Components/Comments/Comments.js
+++ recipe-recommender/src/Components/Comments/Comments.js
@@ -1,47 +1,54 @@
-import React, { useState } from 'react'
-import "../Comments/Comments.css"
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import FavoriteRoundedIcon from '@mui/icons-material/FavoriteRounded';
-import StarRating from '../Middle/StarRating';
+import React, { useState } from "react";
+import "../Comments/Comments.css";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteRoundedIcon from "@mui/icons-material/FavoriteRounded";
+import StarRating from "../Middle/StarRating";
 
-
-const Comments = ( {cmt}) => {
-    const [booleonLike,setBooleonLike] =useState(false)
-    const [likeCount,setLikeCount] =useState(cmt.likes)
+const Comments = ({ cmt }) => {
+  const [booleonLike, setBooleonLike] = useState(false);
+  const [likeCount, setLikeCount] = useState(cmt.likes);
+  console.log(cmt);
+  const getCommentName = (cmt) => {
+    if (cmt.user.username) {
+        return cmt.user.username;
+    }
+    return cmt.user
+  };
   return (
     <div className="overAllCommentList">
-        <div className="commentList">
-            <div className='commentList1'>
-                <div className="commentHead">
-                    {/* <div><img src={cmt.profilePic} /></div> */}
-                    <p></p>
-                    <p><span>{cmt.user.username}</span>{cmt.comment}<span></span></p>
-                </div>
+      <div className="commentList">
+        <div className="commentList1">
+          <div className="commentHead">
+            {/* <div><img src={cmt.profilePic} /></div> */}
+            <p></p>
+            <p>
+              <span>{getCommentName(cmt)}</span>
+              {cmt.comment}
+              <span></span>
+            </p>
+          </div>
 
-                <div className="commentFooter">
-                    {/* <p>{cmt.datetime}</p>
+          <div className="commentFooter">
+            {/* <p>{cmt.datetime}</p>
                     <h4>{booleonLike ? likeCount +1 : likeCount} likes</h4> */}
-                </div>
-            </div>
+          </div>
+        </div>
 
         <div className="commentList2">
-            {/* <p
+          {/* <p
             className='cp'
             onClick={()=>setBooleonLike(!booleonLike)}
             style={{cursor:"pointer"}}
             >
                 {booleonLike ? <FavoriteRoundedIcon /> : <FavoriteBorderOutlinedIcon />}
             </p> */}
-            <div className="star">
-            <StarRating currentRating={cmt.score}/>
-            </div>
-
-
+          <div className="star">
+            <StarRating currentRating={cmt.score} />
+          </div>
         </div>
+      </div>
     </div>
-    </div>
+  );
+};
 
-  )
-}
-
-export default Comments
+export default Comments;
--- recipe-recommender/src/Components/Create/RecipeForm.js
+++ recipe-recommender/src/Components/Create/RecipeForm.js
@@ -127,21 +127,6 @@ const RecipeForm = ({ handleFormSubmit }) => {
               onChange={(e) => setDescription(e.target.value)}
             />
           </div>
-
-          <div class="mb-6">
-            <h1 className="text-lg leading-6 font-medium text-gray-900">
-              Post Image
-            </h1>
-            <input
-              type="text"
-              name="title"
-              id="title"
-              className="shadow-sm p-2 focus:outline-none focus:ring-teal-500 focus:border-teal-500 mt-1 block w-full border border-gray-300 rounded-md"
-              placeholder="Feel free to upload a image URL"
-              value={picture}
-              onChange={(e) => setPicture(e.target.value)}
-            />
-          </div>
           <Ingredients
             editMode={true}
             recipe={ingredients}
@@ -232,6 +217,20 @@ const RecipeForm = ({ handleFormSubmit }) => {
                     onChange={(e) => setCaption(e.target.value)}
                   />
                 </div>
+                <div class="mb-6">
+            <h1 className="text-lg leading-6 font-medium text-gray-900">
+              Post Image
+            </h1>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              className="shadow-sm p-2 focus:outline-none focus:ring-teal-500 focus:border-teal-500 mt-1 block w-full border border-gray-300 rounded-md"
+              placeholder="Feel free to upload a image URL"
+              value={picture}
+              onChange={(e) => setPicture(e.target.value)}
+            />
+          </div>
               </div>
               <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                 <button
